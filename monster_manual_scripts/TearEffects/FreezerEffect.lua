90456505553d476cc39201fe67f91937612eb8d9
thicco-catto <cristobal.martin01@gmail.com>
2023-04-14 10:05:45 +0200

    fix: Fixed not working when the familiar turns permanent

--- monster_manual_scripts/TearEffects/FreezerEffect.lua
+++ monster_manual_scripts/TearEffects/FreezerEffect.lua
@@ -5,14 +5,14 @@ local Constants = require("monster_manual_scripts.Constants")
 TearEffect.AddTearEffect(
     Constants.TearEffects.FREEZING,
     function (_, tear, stats, rng)
-        local baseChance = 15 + stats.Luck * 3
+        local baseChance = 20 + stats.Luck * 4
         if rng:RandomInt(100) < baseChance then
             tear:AddTearFlags(TearFlags.TEAR_FREEZE)
             tear:ChangeVariant(TearVariant.ICE)
         end
     end,
     function (_, laser, stats, rng)
-        local baseChance = 15 + stats.Luck * 3
+        local baseChance = 20 + stats.Luck * 4
         if rng:RandomInt(100) < baseChance then
             laser:AddTearFlags(TearFlags.TEAR_FREEZE)
         end
--- monster_manual_scripts/Upgrades/FireRateUpgrade.lua
+++ monster_manual_scripts/Upgrades/FireRateUpgrade.lua
@@ -3,6 +3,7 @@ local FamiliarUpgrade = require("monster_manual_scripts.FamiliarUpgrade")
 FamiliarUpgrade.NewRepeatableFamiliarUpgrade(
     "tearsup",
     function (_, stats)
-        stats.FireRate = stats.FireRate - 3
+        local tearsUp = math.ceil(stats.FireRate * 0.1)
+        stats.FireRate = stats.FireRate - tearsUp
     end
 )
